dc61db1515846984cc7d7db61011e3f3a4f345ac
luminous3941 <luminous3941@gmail.com>
2025-03-13 02:19:11 +0100

    dashboard

--- frontend/src/Pages/Admin/Dashboard.jsx
+++ frontend/src/Pages/Admin/Dashboard.jsx
@@ -26,7 +26,7 @@ const Dashboard = () => {
 
   return (
     <div className="h-full flex bg-gradient-to-tr from-black via-gray-900 to-gray-800 text-white">
-      {/* Sidebar */}
+      
       <div className="w-[20%] bg-gray-800 p-6 mt-20 flex flex-col space-y-4 h-screen fixed">
         <h2 className="text-2xl font-bold text-purple-400 text-center mb-2">Dashboard</h2>
         <button className="w-full py-2 bg-gray-700 rounded-md hover:bg-purple-600 scale">
@@ -44,8 +44,6 @@ const Dashboard = () => {
       </div>
 
       
-
-      {/* Main Content */}
       <div className="flex-1 pt-16 ml-[20%]">
         <div className="flex flex-col justify-start px-10">
           <p className="text-start mt-4 text-white text-xl">
